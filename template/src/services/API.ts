870a50a7b09f3f1639611ae61123fb3ad90412c6
faraj Shuaib <46637985+farajshuaib@users.noreply.github.com>
2022-01-28 00:03:54 +0200

    update axios file

--- template/src/services/API.ts
+++ template/src/services/API.ts
@@ -1,73 +1,23 @@
 import axios from "axios";
-import { AuthData } from "../types";
 
 export const baseURL = "";
 
-class Axios {
-  // Axios Instances ..
-  Axios; // Authenticated Auto Refresh Axios
+const token = localStorage.getItem("token") || "";
 
-  currentToken = ""; // Current Token, Loaded from Local Storage
+const Axios = axios.create({
+  baseURL,
+  headers: {
+    Authorization: `Bearer ${token}`,
+    Accept: "application/json",
+  },
+});
 
-  constructor() {
-    this.Axios = axios.create({ baseURL });
-
-    this.getToken();
-
-    // Add The Interceptors .. ( The Auth Interceptor is also used for logging .. )
-    this.Axios.interceptors.request.use(this.requestAuthInterceptor);
-
-    this.Axios.interceptors.response.use(
-      (response) => response,
-      this.requestErrorInterceptor
-    );
-  }
-
-  async getToken() {
-    this.currentToken = localStorage.getItem("token") || "";
-  }
-
-  requestAuthInterceptor = (request: any) => {
-    request.headers.Authorization = "Bearer " + this.currentToken;
+// set yyor token here by call back this function after login... 
+export const setBearerToken = (token: string) =>
+  Axios.interceptors.request.use((request: any) => {
+    localStorage.setItem("token", token);
+    request.headers.Authorization = "Bearer " + token;
     return request;
-  };
+  });
 
-  requestErrorInterceptor = (error: any) => {
-    return Promise.reject(error);
-  };
-
-  async saveAuthData(token: string) {
-    this.currentToken = token;
-    localStorage.setItem("token", token.toString());
-  }
-  clearLocalStorage() {
-    this.currentToken = "";
-    localStorage.clear();
-  }
-  async authenticate() { // getting the user data from the server whenever lose it and he's already logged in .
-    return this.Axios.get("is_logged_in");
-  }
-  async login(authData: AuthData) {
-    const response = await this.Axios.post("login", authData);
-    console.log(response);
-    if (response.data.data) {
-      this.saveAuthData(response.data.data.token);
-    }
-    return response;
-  }
-  async logout() {
-    try {
-      const response = await this.Axios.get(`logout`);
-      if (response.status === 200 || response.status === 204) {
-        this.clearLocalStorage();
-      }
-      return response;
-    } catch (err) {
-      throw err;
-    }
-  }
-}
-
-const API = new Axios();
-
-export default API;
+export default Axios;
