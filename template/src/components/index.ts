1bd8286969ad86b5685fd7e606379153d6aee456
faraj Shuaib <46637985+farajshuaib@users.noreply.github.com>
2022-04-21 02:53:08 +0200

    fix bugs

--- template/src/components/index.ts
+++ template/src/components/index.ts
@@ -1,6 +1,6 @@
 // import all your component from sub components folders here to export it from one src...
 import Layout from "./Layout";
-import LoadingScreen from "./LoadingScreen";
+import LoadingScreen from "./utils/LoadingScreen";
 
 
 export { Layout, LoadingScreen };
